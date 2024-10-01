afb75dc4ab718dda733a591be2154d6eaead6346
Aurellia Alya <aaurellia23>
2024-10-01 20:34:48 +0700

    hasil modifikasi tahun tabiat

--- Pemilihan2Percobaan103.java
+++ Pemilihan2Percobaan103.java
@@ -1,19 +1,23 @@
 import java.util.Scanner;
+
 public class Pemilihan2Percobaan103 {
     public static void main(String[] args) {
-       Scanner input = new Scanner(System.in);
-       
-       System.out.print("Masukkan tahun: ");
-       int tahun = input.nextInt();
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Masukkan tahun: ");
+        int tahun = input.nextInt();
 
-       System.out.println("Tahun yang dimasukkan adalah: " + tahun);
-       input.close();
+        System.out.println("Tahun yang dimasukkan adalah: " + tahun);
+        input.close();
 
-       if ((tahun % 4) == 0) {
-        if ((tahun % 100) != 0)
-        System.out.println("Tahun Kabisat");
-       } else
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0 || (tahun % 400) == 0) {
+                System.out.println("Tahun Kabisat");
+            } else {
+                System.out.println("Bukan Tahun Kabisat");
+            }
+        } else {
             System.out.println("Bukan Tahun Kabisat");
-
-       }
+        }
     }
+}
