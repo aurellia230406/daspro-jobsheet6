35ceb322fc3c023921b69f177c2a42dcb401fcbf
Aurellia Alya <aaurellia23>
2024-10-01 15:54:26 +0700

    pemilihan percobaan 2

---
+++ Pemilihan2Percobaan103.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Pemilihan2Percobaan103 {
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+       
+       System.out.print("Masukkan tahun: ");
+       int tahun = input.nextInt();
+
+       System.out.println("Tahun yang dimasukkan adalah: " + tahun);
+       input.close();
+
+       if ((tahun % 4) == 0) {
+        if ((tahun % 100) != 0)
+        System.out.println("Tahun Kabisat");
+       } else
+            System.out.println("Bukan Tahun Kabisat");
+
+       }
+    }
