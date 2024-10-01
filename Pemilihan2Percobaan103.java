f038eb7224899b3cf1644ec239df2b72df6934d7
Aurellia Alya <aaurellia23>
2024-10-01 20:41:15 +0700

    Memodifikasi logika tahun kabisat tanpa operator logika

--- Pemilihan2Percobaan103.java
+++ Pemilihan2Percobaan103.java
@@ -10,12 +10,12 @@ public class Pemilihan2Percobaan103 {
         System.out.println("Tahun yang dimasukkan adalah: " + tahun);
         input.close();
 
-        if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0 || (tahun % 400) == 0) {
-                System.out.println("Tahun Kabisat");
-            } else {
-                System.out.println("Bukan Tahun Kabisat");
-            }
+        if (tahun % 400 == 0) {
+            System.out.println("Tahun Kabisat");
+        } else if (tahun % 100 == 0) {
+            System.out.println("Bukan Tahun Kabisat");
+        } else if (tahun % 4 == 0) {
+            System.out.println("Tahun Kabisat");
         } else {
             System.out.println("Bukan Tahun Kabisat");
         }
